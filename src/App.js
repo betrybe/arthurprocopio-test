a472f086927e4552c4a083e440b90d2e8299a8aa
Arthur Procopio <arthur.procopio@udooku.com>
2021-09-24 10:23:41 -0300

    Configure router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { LoginPage, WalletPage } from './pages';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
+        <Route path="/carteira" exact>
+          <WalletPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
