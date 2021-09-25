484e1fa17cbb6566320728ab19803d64e482c1d7
Arthur Procopio <arthur.procopio@udooku.com>
2021-09-25 20:14:28 -0300

    Add login page with handlers

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,67 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import LoginForm from '../components/LoginForm';
+import { authenticate } from '../actions';
+import './Login.css';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.onAuthSubmit = props.onAuthSubmit;
+    this.isLoading = props.isLoading;
+
+    this.handleChangeForm = this.handleChangeForm.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = this.state;
+    console.log(email);
+    console.log(password);
+    this.onAuthSubmit(email);
+  }
+
+  handleChangeForm(event) {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
   render() {
-    return <div>Login</div>;
+    return (
+      <div className="wraper">
+        <div className="form-container text-center">
+          <LoginForm
+            handleSubmit={ this.handleSubmit }
+            handleChange={ this.handleChangeForm }
+            formValues={ this.state }
+            isLoading={ this.isLoading }
+          />
+        </div>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  onAuthSubmit: (email) => {
+    dispatch(authenticate({ email }));
+  },
+});
+
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  isLoading: state.user.loading,
+});
+
+Login.propTypes = {
+  onAuthSubmit: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
