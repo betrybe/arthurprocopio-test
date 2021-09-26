16ca6209a120551f55039b1c4777c0683ef3fcab
Arthur Procopio <arthur.procopio@udooku.com>
2021-09-26 13:44:23 -0300

    Keep store as advised by project decription

--- src/store.js
+++ src/store.js
@@ -3,14 +3,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-export default function configureAppStore() {
-  const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(thunk)),
-  );
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
 
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
-    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer));
-  }
-  return store;
-}
+export default store;
