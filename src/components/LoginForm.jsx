5a2842f13ed6ea123c5be050872038f1cb15101f
Arthur Procopio <arthur.procopio@udooku.com>
2021-09-26 13:43:10 -0300

    Add validation to login form component

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,51 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './LoginForm.css';
+import TextField from './TextField';
 
 function LoginForm(props) {
   const {
     handleSubmit,
     handleChange,
-    formValues,
+    formState,
+    isLoading,
   } = props;
+  const hasError = (field) => (!!(formState.touched[field] && formState.errors[field]));
+  const errorText = (field) => (formState.errors[field] ? formState.errors[field][0] : '');
   return (
     <form className="form-signin" onSubmit={ handleSubmit }>
       {/* TODO add image <img className="mb-4" src="" alt="" width="72" height="72" /> */}
-      <h1 className="h3 mb-3 font-weight-normal">Entrar</h1>
-      <input
+      <TextField
         type="email"
         id="inputEmail"
         name="email"
-        className="form-control"
         placeholder="Email"
         onChange={ handleChange }
-        value={ formValues.email || '' }
-        data-testid="email-input"
+        value={ formState.values.email || '' }
+        dataTestId="email-input"
+        hasError={ hasError('email') }
+        errorText={ errorText('email') }
         required
       />
-      <input
+      <TextField
         type="password"
         id="inputPassword"
         name="password"
-        className="form-control"
         placeholder="Senha"
         onChange={ handleChange }
-        value={ formValues.password || '' }
-        data-testid="password-input"
+        value={ formState.values.password || '' }
+        dataTestId="password-input"
+        hasError={ hasError('password') }
+        errorText={ errorText('password') }
         required
       />
-      <button className="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      {
+        isLoading && <div className="spinner-border" role="status" />
+      }
+      <button
+        className="btn btn-lg btn-primary btn-block"
+        type="submit"
+        disabled={ !formState.isValid || isLoading }
+      >
+        Entrar
+      </button>
     </form>
   );
 }
 
+LoginForm.defaultProps = {
+  isLoading: false,
+};
+
 LoginForm.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
   handleChange: PropTypes.func.isRequired,
-  formValues: PropTypes.shape({
-    email: PropTypes.string,
-    password: PropTypes.string,
+  formState: PropTypes.shape({
+    isValid: PropTypes.bool,
+    values: PropTypes.shape({
+      email: PropTypes.string,
+      password: PropTypes.string,
+    }),
+    touched: PropTypes.object,
+    errors: PropTypes.object,
   }).isRequired,
+  isLoading: PropTypes.bool,
 };
 
 export default LoginForm;
